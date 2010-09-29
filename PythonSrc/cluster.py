f08c59015b7e30338177e17654e02aa992552249
Thierry Bertin-Mahieux <tb2332@columbia.edu>
2010-09-29 17:18:57 -0400

    mostly to do kmeans on btchromas

---
+++ PythonSrc/cluster.py
@@ -0,0 +1,9 @@
+"""
+Set of functions to do clustering, usually kmeans, on a bunch
+of btcroma.
+
+It reproduces our ISMIR 2010 paper, wihtout the online thing.
+
+T. Bertin-Mahieux (2010) Columbia University
+tb2332@columbia.edu
+"""
