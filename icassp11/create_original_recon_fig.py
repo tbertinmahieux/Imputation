90c6afe58f8b85309e125170d0d0ab30896bf127
Thierry Bertin-Mahieux <tb2332@columbia.edu>
2010-10-19 11:00:31 -0400

    update

--- icassp11/create_original_recon_fig.py
+++ icassp11/create_original_recon_fig.py
@@ -35,11 +35,12 @@ recon,tmp = IMPUTATION.lintransform_patch(bt,mask,p1,p2,win=1)
 errs = evaluation.recon_error(bt,mask,recon)
 s = 'method: linear transform\n'
 s += 'eucl = ' + nice_nums(errs['eucl']) + '\n'
+s += 'd1/2 = ' + nice_nums(errs['lhalf']) + '\n'
 s += 'D-ENT = ' + nice_nums(errs['dent']) + '\n'
 s += 'Jensen diff. = ' + nice_nums(errs['jdiff']) + '\n'
 s += 'Levenshtein. = ' + nice_nums(errs['leven']) + '\n'
 originals.append( bt[:,p1:p2].copy() )
-recons.append( recon.copy() )
+recons.append( recon[:,p1:p2].copy() )
 texts.append(s)
 
 
@@ -50,12 +51,31 @@ recon,tmp = IMPUTATION.knn_patch(bt,mask,p1,p2,measure='eucl',win=8)
 errs = evaluation.recon_error(bt,mask,recon)
 s = 'method: NN\n'
 s += 'eucl = ' + nice_nums(errs['eucl']) + '\n'
+s += 'd1/2 = ' + nice_nums(errs['lhalf']) + '\n'
 s += 'D-ENT = ' + nice_nums(errs['dent']) + '\n'
 s += 'Jensen diff. = ' + nice_nums(errs['jdiff']) + '\n'
 s += 'Levenshtein. = ' + nice_nums(errs['leven']) + '\n'
 originals.append( bt[:,p1:p2].copy() )
-recons.append( recon.copy() )
+recons.append( recon[:,p1:p2].copy() )
 texts.append(s)
+
+
+# RANDOM RECON
+bt = sio.loadmat('/home/thierry/Columbia/covers80/coversongs/covers32k/Caroline_No/beach_boys+Pet_Sounds+13-Caroline_No.mp3.mat')['btchroma']
+mask,p1,p2 = masking.random_patch_mask(bt,ncols=15,win=25)
+recon = IMPUTATION.random_patch(bt,mask,p1,p2)
+errs = evaluation.recon_error(bt,mask,recon)
+s = 'method: Random\n'
+s += 'eucl = ' + nice_nums(errs['eucl']) + '\n'
+s += 'd1/2 = ' + nice_nums(errs['lhalf']) + '\n'
+s += 'D-ENT = ' + nice_nums(errs['dent']) + '\n'
+s += 'Jensen diff. = ' + nice_nums(errs['jdiff']) + '\n'
+s += 'Levenshtein. = ' + nice_nums(errs['leven']) + '\n'
+originals.append( bt[:,p1:p2].copy() )
+recons.append( recon[:,p1:p2].copy() )
+texts.append(s)
+
+
 
 # done, plot
 assert len(originals) == len(recons)
@@ -71,9 +91,14 @@ for k in range(len(originals)):
     # original
     P.subplot(len(originals),3,k*3+1)
     P.imshow(originals[k],**pargs)
+    if k + 1 < len(originals):
+        P.xticks(())
     # recon
-    P.subplot(len(originals),3,k*3+2)
-    P.imshow(originals[k],**pargs)
+    P.subplot(len(recons),3,k*3+2)
+    P.imshow(recons[k],**pargs)
+    if k + 1 < len(originals):
+        P.xticks(())
+    P.yticks(())
     # text
     P.subplot(len(originals),3,k*3+3)
     P.gca().set_axis_off()# remove black box around it
@@ -81,12 +106,10 @@ for k in range(len(originals)):
     P.xticks(())
     P.yticks(())
     width = 1    # out of 10
-    height = 8  # out of 10
+    height = -1  # out of 10
     lines = filter(lambda s: s!='',texts[k].split('\n'))
-    for lid,line in enumerate(lines):
-        P.text(width,height-lid, line, fontdict=None)
-               #horizontalalignment='center',
-               #verticalalignment='center')
+    P.text(width,height, texts[k], fontdict=None)
+
 
 P.show()
 
