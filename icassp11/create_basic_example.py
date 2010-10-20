e1feefe44578a9611c1006311f52a2efb4b90375
Thierry Bertin-Mahieux <tb2332@columbia.edu>
2010-10-20 11:03:53 -0400

    create_basic_example.py and other figures

---
+++ icassp11/create_basic_example.py
@@ -0,0 +1,12 @@
+"""
+Code to create the first figure, where we do reconstruction using a set
+of methods and report some measures.
+"""
+
+import os
+import sys
+import numpy as np
+import scipy.io as sio
+
+sys.path.append('/home/thierry/Columbia/Imputation/PythonSrc')
+import imputation as IMPUTATION
